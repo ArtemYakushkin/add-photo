ceda15205044c0c06414ba71bd9305c95be4d7af
ArtemYakushkin <zimberger29101985@gmail.com>
2022-05-17 12:28:26 +0300

    add sections services, stages, motivation and add bg-video in hero

---
+++ js/motivation.js
@@ -0,0 +1,17 @@
+function readMore() {
+    const dots = document.getElementById('dots');
+    const more = document.getElementById('more');
+    const btn = document.getElementById('btn');
+
+    if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        btn.innerHTML = 'Подробнее';
+        more.style.display = 'none';
+    } else {
+        dots.style.display = 'none';
+        btn.innerHTML = 'Скрыть';
+        more.style.display = 'inline';
+    }
+}
+
+
